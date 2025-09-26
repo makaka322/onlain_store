9bb1078a16a68b74bd8eb33be260558b684ef610
Makaka322 <chirik2000xd@mail.ru>
2025-09-26 15:50:19 +0800

    Make Home_work_23

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 from django.conf.global_settings import STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'onlain_store',
+        'USER': 'postgres',
+        'PASSWORD': '487369',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -126,3 +130,6 @@ STATICFILES_DIRS = (BASE_DIR/'static',)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
