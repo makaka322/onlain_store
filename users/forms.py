a6bd7612296a3fd89062e3b8e9f52321a9aa6954
Makaka322 <chirik2000xd@mail.ru>
2025-10-10 23:25:13 +0800

    Make Homework-27

---
+++ users/forms.py
@@ -0,0 +1,10 @@
+from users.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from catalog.forms import StyleFormMixin
+
+
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ("email", "password1", "password2")
