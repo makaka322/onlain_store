87e8eb968e4889edd5cfa039b284ec70d245e2e7
Makaka322 <chirik2000xd@mail.ru>
2025-10-13 20:34:21 +0800

    Make Homework-28_1

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
+from django.views import View
 from django.views.generic import ListView, DetailView, TemplateView,  CreateView, DeleteView, UpdateView
 
 from catalog.forms import ProductForm
@@ -20,9 +23,19 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     form_class = ProductForm
     success_url = reverse_lazy("catalog:product_list")
 
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     success_url = reverse_lazy("catalog:product_list")
+
+    def get_object(self, queryset=None):
+        product = super().get_object(queryset)
+        if product.owner != self.request.user:
+            raise PermissionDenied("Вы не можете удалять этот продукт.")
+        return product
 
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
@@ -30,7 +43,24 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
     form_class = ProductForm
     success_url = reverse_lazy("catalog:product_list")
 
+    def get_object(self, queryset=None):
+        product = super().get_object(queryset)
+        if product.owner != self.request.user:
+            raise PermissionDenied("Вы не можете редактировать этот продукт.")
+        return product
+
     def get_success_url(self):
         return reverse("catalog:product_detail", args=[self.kwargs.get("pk")])
 
+class UnpublishProductView(LoginRequiredMixin, View):
 
+    def post(self, request, pk):
+        product = get_object_or_404(Product, id=pk)
+
+        if not request.user.has_perm("catalog.can_publish_product"):
+            raise PermissionDenied("У вас нет права на публикацию продукции")
+
+        product.is_published = True
+        product.save()
+
+        return redirect("catalog:product_detail", pk=pk)
